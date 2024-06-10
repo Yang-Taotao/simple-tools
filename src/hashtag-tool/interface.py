4873e7cd592696d0ebfaa2da5bac7793fa4a2f4c
Yang Taotao <74541908+Yang-Taotao@users.noreply.github.com>
2024-06-10 12:48:21 +0100

    Update GUI styling

--- src/hashtag-tool/interface.py
+++ src/hashtag-tool/interface.py
@@ -16,23 +16,24 @@ def gui_convert():
     """
     txt_input = input_entry.get()
     txt_output = converter.convert(txt_input)
-    label_output.config(text=txt_output)
+    output_entry.config(text=txt_output)
 
 
 def gui_copy():
     """
     GUI operation to copy the converted result
     """
-    txt_output = label_output.cget("text")
+    txt_output = output_entry.cget("text")
     root.clipboard_clear()
     root.clipboard_append(txt_output)
 
 
 def gui_clear():
     """
-    GUI operation to clear the input entry
+    GUI operation to clear the input and ouput entries
     """
     input_entry.delete(0, tk.END)
+    output_entry.delete(0, tk.END)
 
 
 # GUI build
@@ -40,25 +41,38 @@ root = tk.Tk()
 root.title("Hashtag Converter")
 
 # GUI style
-font = Font(family="Arial", size=12)
+root.geometry("800x600")
+font = Font(family="Arial", size=16)
 
 # Widgets - input
-label_input = tk.Label(root, text="Enter your hashtags here:")
-label_input.grid(row=0, column=0, padx=5, pady=5)
-input_entry = tk.Entry(root)
-input_entry.grid(row=0, column=1, padx=5, pady=5)
+label_input = tk.Label(root, text="Enter Your\n Hashtags Here:", font=font)
+label_input.grid(row=0, column=0, padx=(10, 5), pady=10, sticky="we")
+input_entry = tk.Entry(root, font=font)
+input_entry.grid(row=0, column=1, padx=5, pady=10, sticky="we")
 
 # Buttons
-button_convert = tk.Button(root, text="Convert", command=gui_convert)
-button_convert.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
-button_copy = tk.Button(root, text="Copy", command=gui_copy)
-button_copy.grid(row=3, column=0, padx=5, pady=5)
+button_convert = tk.Button(root, text="Convert", font=font, command=gui_convert)
+button_convert.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="we")
+button_copy = tk.Button(root, text="Copy", font=font, command=gui_copy)
+button_copy.grid(row=3, column=0, padx=10, pady=10, sticky="we")
 button_clear = tk.Button(root, text="Clear", font=font, command=gui_clear)
-button_clear.grid(row=3, column=1, padx=5, pady=5)
+button_clear.grid(row=3, column=1, padx=10, pady=10, sticky="we")
 
 # Widget - output
-label_output = tk.Label(root, text="")
-label_output.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+label_output = tk.Label(root, text="Converted\n Hashtags:", font=font)
+label_output.grid(row=2, column=0, padx=(10, 5), pady=10, sticky="we")
+output_entry = tk.Text(root, wrap="word", height=4)
+output_entry.grid(row=2, column=1, padx=5, pady=10, sticky="we")
+
+# GUI adaptive style
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
+root.grid_rowconfigure(2, weight=1)
+root.grid_rowconfigure(3, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
 
 # GUI ops
 root.mainloop()
+
+# %%
