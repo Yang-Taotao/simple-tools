b65c080b14771927a1ce0743947cbba636cd0e81
Yang Taotao <74541908+Yang-Taotao@users.noreply.github.com>
2024-08-05 15:30:19 +0100

    Refactor

--- src/weather-tool/weather-tool.py
+++ src/weather-tool/weather-tool.py
@@ -18,8 +18,14 @@ KELVIN = 273.15
 
 
 def weather_get():
+    """Get weather data from web API
+
+    Returns:
+        data (dict): weather data dict
+    """
     # Build URL
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME},{COUNTRY_CODE}&appid={API_KEY}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={
+        CITY_NAME},{COUNTRY_CODE}&appid={API_KEY}"
     # Get data
     response = requests.get(url)
     # Check status
@@ -35,17 +41,42 @@ def weather_get():
 
 
 def weather_read(data):
+    """Read weather data
+
+    Args:
+        data (dict): weather data dict
+
+    Returns:
+        temp_min (float): min temperature
+        temp_max (float): max temperature
+        desc (str): weather description
+        rain (float): chance of rain
+    """
     # Read weather data
     temp_min, temp_max, desc, rain = (
         data['main']['temp_min'],   # Temperature minimum
         data['main']['temp_max'],   # Temperature maximum
         data['weather'][0]['description'],  # Weather description
-        data.get('rain', {}).get('1h', 0) # Chance of rain in the next hour
+        data.get('rain', {}).get('1h', 0)  # Chance of rain in the next hour
     )
+    # Convertion
+    temp_min, temp_max = temp_min-KELVIN, temp_max-KELVIN
+    # Func return
     return temp_min, temp_max, desc, rain
 
 
+# Data - display
+
+
 def weather_print(temp_min, temp_max, desc, rain):
+    """Display weather results
+
+    Args:
+        temp_min (float): converted min temperature
+        temp_max (float): converted max temperature
+        desc (str): weather description
+        rain (float): chance of rain in the next hour
+    """
     # Get current date and time
     date = datetime.now().strftime('%Y-%m-%d')
     time = datetime.now().strftime('%H:%M-%S')
@@ -56,22 +87,25 @@ def weather_print(temp_min, temp_max, desc, rain):
     print(f"{'Inquiry Time':<20}{time:>10}")
     print("-" * 30)
     print(f"{'LOCATION':<10}{CITY_NAME:>20}")
-    print(f"{'LOW':<5}{temp_min:>22.2f}{'\u00B0C':>3}")
-    print(f"{'HIGH':<5}{temp_max:>22.2f}{'\u00B0C':>3}")
+    print(f"{'TEMP LOW':<10}{temp_min:>17.2f}{'\u00B0C':>3}")
+    print(f"{'TEMP HIGH':<10}{temp_max:>17.2f}{'\u00B0C':>3}")
     print(f"{'TYPE':<5}{desc:>25}")
-    print(f"{'RAIN':<5}{rain:>25.2f}")
+    print(f"{'RAIN 1H':<10}{rain:>20.2f}")
     print("=" * 30)
 
 
+# Main func
+
+
 def weather_tool():
+    """
+    Main weather tool func
+    """
     # Get data
     data = weather_get()
-    temp_min, temp_max, desc, rain = weather_read(data)
-    # Convertion
-    temp_min, temp_max = temp_min-KELVIN, temp_max-KELVIN
     # Display
-    weather_print(temp_min, temp_max, desc, rain)
-    
+    weather_print(*weather_read(data))
+
 
 # OPS
 if __name__ == '__main__':
